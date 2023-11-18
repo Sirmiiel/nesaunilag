2a220be6a78f1516b8bfc4506b566ee4cd60dc84
Samuel Aribigbola <ayoolasamuel2302@gmail.com>
2023-11-18 12:09:38 +0100

    final commit

--- javascript/upload.js
+++ javascript/upload.js
@@ -1,26 +1,64 @@
 
-// Image Upload Box for Admin
-const dropArea = document.getElementById("drop-area");
-const inputFile = document.getElementById("input-file");
-const imageView = document.getElementById("img-view");
+/*
 
-inputFile.addEventListener("change", uploadImage);
+whatsapp: https://api.whatsapp.com/send?text=[post-title] [post-url]
 
-function uploadImage(){
-    let imgLink = URL.createObjectURL(inputFile.files[0]);
-    imageView.style.backgroundImage = `url(${imgLink})`;
-    imageView.textContent = "";
+
+
+
+Twitter social share URL:
+
+https://twitter.com/share?url=[post-url]&text=[post-title]
+
+
+
+
+LinkedIn social share URL:
+
+https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+
+
+
+
+Facebook social share URL:
+
+https://www.facebook.com/sharer.php?u=[post-url]
+
+
+
+*/  
+
+const facebookBtn = document.querySelector(".facebook-btn");
+const linkedinBtn = document.querySelector(".linkedin-btn");
+const twitterBtn = document.querySelector(".twitter-btn");
+const whatsappBtn = document.querySelector(".whatsapp-btn");
+
+
+function init() {
+    let postUrl = encodeURI(document.location.href);
+    let postTitle = encodeURI("Check out this post from NESA UNILAG: ")
+
+    facebookBtn.setAttribute(
+        "href", 
+        `https://www.facebook.com/sharer.php?u=${postUrl}`
+    );
+
+    twitterBtn.setAttribute(
+        "href", 
+        `https://twitter.com/share?url=${postUrl}&text=${postTitle}`
+    );
+
+    whatsappBtn.setAttribute(
+        "href", 
+        `https://api.whatsapp.com/send?text=${postTitle} ${postUrl}`
+    );
+
+    linkedinBtn.setAttribute(
+        "href", 
+        `https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`
+    );
 }
 
 
-dropArea.addEventListener("dragover", function(e){
-    e.preventDefault();
-});
-dropArea.addEventListener("drop", function(e){
-    e.preventDefault();
-    inputFile.files = e.dataTransfer.files;
-    uploadImage();
-});
-
-
+init();
 
